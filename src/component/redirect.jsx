24ef292e57c61b6af3f02974cf9d099ffd2d6a8e
ydkdan6 <ydkdan6@gmail.com>
2024-03-21 18:49:08 -0700

    Fixed view port

--- src/component/redirect.jsx
+++ src/component/redirect.jsx
@@ -8,7 +8,7 @@ export default function HandleNextPage() {
         <button className="bg-blue-500 w-80 h-12 rounded-lg">Get Started</button>
         </Link>
         <Link to="/Login">
-      <button className="bg-white-500 w-80 h-12v text-black mt-3 rounded-lg">
+      <button className="bg-white-500 w-80 h-12 text-black mt-3 rounded-lg">
         Login
       </button>
       </Link>
--- src/page/landingPage.js
+++ src/page/landingPage.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import HandleNextPage from '../component/redirect';
-import image1 from './images/Credit-card.jpg';
+import image1 from './images/christ.jpg';
+import '../Utility/style/style.css'
 
 
 export default function LandingPage() {
     
     return (
-        <div className= "container">
+        <div className= "container w-76">
             <div className="flex justify-center items-center mt-2">
                 <img src={image1} alt= "Banner-image" />
             </div>
             <section className="justify-center items-center mt-1">
                 <div className="flex flex-col items-center">
-                <h2 className="w-46 text-bold text-2xl font-black text-black ">Swift, seamless global <br/>financial transactions.</h2>
-                <p className="justify-center w-80 mt-4">World's fastest payment method for swift</p>
-                <p>and efficient transactions</p>
+                <h2 className="txt mt-3 text-bold text-2xl items-center font-black text-black ">Search Bible Verses,  Join Our Community.</h2>
+                <p className=" txt2 justify-center w-80 mt-9">Join Us in Biblimo, Let us build a Christian Community,
+                Where the knowledge of Christ is being spread.</p>
                 <HandleNextPage />
                 </div>
             </section>
